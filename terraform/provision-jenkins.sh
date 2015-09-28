25bb4605487cfe991e103d0156c59a1f1d80435d
Cloud User <centos@ip-10-194-154-114.us-west-2.compute.internal>
2015-09-28 17:57:42 +0000

    hZZ

--- terraform/provision-jenkins.sh
+++ terraform/provision-jenkins.sh
@@ -1,31 +1,37 @@
-sudo yum update
+#sudo yum -y update
 
 #Java
-sudo yum -y install java-1.7.0-openjdk
+#sudo yum -y install java-1.7.0-openjdk
 
 # Jenkins
-sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
-sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-sudo yum -y install jenkins
+#sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+#sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+#sudo yum -y install jenkins
 
 # Docker
-curl -sSL https://get.docker.com/ | sh
+if [rpm -qa | grep docker | wc -l > 0]; then
+   echo "Docker already installed, skipping..."
+else
+   curl -sSL https://get.docker.com/ | sh
+fi
+
 sudo service docker start
 sudo chkconfig docker on
 
-pushd .
-cd /tmp
-sudo wget https://nodejs.org/dist/v4.1.1/node-v4.1.1-linux-x64.tar.gz
-sudo tar --strip-components 1 -xzvf node-v* -C /usr/local
 
-#Fix the links so sudo npm works:
-sudo ln -s /usr/local/bin/node /usr/bin/node
-sudo ln -s /usr/local/lib/node /usr/lib/node
-sudo ln -s /usr/local/bin/npm /usr/bin/npm
-sudo ln -s /usr/local/bin/node-waf /usr/bin/node-waf
-
+#pushd .
+#cd /tmp
+#sudo wget https://nodejs.org/dist/v4.1.1/node-v4.1.1-linux-x64.tar.gz
+#sudo tar --strip-components 1 -xzvf node-v* -C /usr/local
+#
+##Fix the links so sudo npm works:
+#sudo ln -s /usr/local/bin/node /usr/bin/node
+#sudo ln -s /usr/local/lib/node /usr/lib/node
+#sudo ln -s /usr/local/bin/npm /usr/bin/npm
+#sudo ln -s /usr/local/bin/node-waf /usr/bin/node-waf
+##
 # install Mocha
-sudo npm install -g mocha
+#sudo npm install -g mocha
 
 # install Junit Reporter
-sudo npm install -g mocha-junit-reporter
+#sudo npm install -g mocha-junit-reporter
