2127b2f62bed8e6a65bd8d097a36a9ecc7366923
Watkins, Andrew <Andrew.Watkins@move.com>
2015-09-29 07:45:21 -0700

    tf can work for sdswrite_dev or geo_dev

--- terraform/jenkins-setup.tf
+++ terraform/jenkins-setup.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    access_key = "ASIAJOWTZMPFFRJGMWYQ"
-    secret_key = "vXj8sHPLYDs7qkgFCVUc93XUDnr7wxcmzK8LAfKX"
+    access_key = "ASIAIHCCHWDSLV736OSQ"
+    secret_key = "Q2eQUinEGuVWDq1Ekc4kZg+NfwoxllpAQco944HY"
     region = "us-west-2"
 }
 
@@ -14,4 +14,16 @@ resource "aws_instance" "example" {
     tags {
         Name = "aw-terraform-test"
     }
+
+#resource "aws_instance" "example" {
+#    count = 1
+#    instance_type = "t2.micro"
+#    ami = "ami-49d0cd79"
+#    security_groups = ["sg-46d0e323"]
+#    subnet_id = "subnet-c05af3b7"
+#    key_name = "AndrewsAwsKeyPair"
+#    tags {
+#        Name = "aw-terraform-test"
+#    }
 }
+
