9207409f2d249e11d7463e5c0c9a76955f0e36b8
Andrew Watkins <andrew.watkins@move.com>
2015-10-01 19:08:18 -0700

    no message

--- soa-system-def/terraform/userdata_node_provision.sh
+++ soa-system-def/terraform/userdata_node_provision.sh
@@ -11,7 +11,7 @@
 echo "***** docker startup" >> /var/log/terraform.log
 #systemctl start docker.service >> /var/log/terraform.log
 #systemctl enable docker.service >> /var/log/terraform.log
-service docker start 
+#service docker start 
 
 #echo "***** docker installing ecs-agent" >> /var/log/terraform.log
 #docker run --name ecs-agent --detach=true --restart=on-failure:10 --volume=/var/run/docker.sock:/var/run/docker.sock --volume=/var/log/ecs/:/log --volume=/var/lib/ecs/data:/data --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro --volume=/var/run/docker/execdriver/native:/var/lib/docker/execdriver/native:ro --publish=127.0.0.1:51678:51678 --env=ECS_LOGFILE=/log/ecs-agent.log --env=ECS_LOGLEVEL=info --env=ECS_DATADIR=/data --env=ECS_CLUSTER=${ecs_cluster} amazon/amazon-ecs-agent:latest
