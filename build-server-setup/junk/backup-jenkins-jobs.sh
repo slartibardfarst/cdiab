66201e10d418a7bd7b7676befd6ed86ce04b4c26
Watkins, Andrew <Andrew.Watkins@move.com>
2015-10-11 00:27:26 -0700

    deleted unused files

--- build-server-setup/junk/backup-jenkins-jobs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-jenkins=$(cat "../jenkins-server-ip.txt")
-
-if [ ! -e "./jenkins-cli.jar" ]; then
-	curl "http://$jenkins:8080/jnlpJars/jenkins-cli.jar" -o "jenkins-cli.jar"
-fi
-
-java -jar jenkins-cli.jar -s http://$jenkins:8080/ list-jobs 
--- build-server-setup/junk/restore-jenkins-jobs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-jenkins=$(cat "../jenkins-server-ip.txt")
-echo $jenkins
-
-
-
-curl -X POST -H "Content-Type:application/xml" -d @"build_api.xml" "http://$jenkins:8080/createItem?name=build_api"
